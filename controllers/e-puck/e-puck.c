ccfcc1cb9f669a283da7f5b0e83448de0cbdeb10
jean <93669153+rauber6@users.noreply.github.com>
2024-11-11 08:37:43 +0100

    topic 4 template

---
+++ controllers/e-puck/e-puck.c
@@ -0,0 +1,16 @@
+//******************************************************************************
+//  Name:   e-puck.c
+//  Author: -
+//  Date:   -
+//  Rev:    -
+//******************************************************************************
+
+#include <webots/robot.h>
+
+int main(int argc, char *argv[]) {
+
+  // controller initialization
+  wb_robot_init(); 
+
+  return 0;
+}
---
+++ controllers/supervisor/supervisor.c
@@ -0,0 +1,18 @@
+//******************************************************************************
+//  Name:   supervisor.c
+//  Author: -
+//  Date: -
+//  Rev: -
+//******************************************************************************
+
+#include <webots/robot.h>
+#include <webots/supervisor.h>
+
+int main(int argc, char *argv[]) {
+
+  /* initialize Webots */
+  wb_robot_init();
+  
+  return 0;
+}
+
