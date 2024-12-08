7e8013f9a5ced85cd9b85aa43a57044d91fc73fa
Hana Catic <hana.catic@epfl.ch>
2024-12-09 00:43:57 +0100

    fixed update_state_custom in epuck lib centralised.

--- controllers/e-puck/e-puck-lib-centralized.cc
+++ controllers/e-puck/e-puck-lib-centralized.cc
@@ -176,7 +176,7 @@ void EpuckCentralized::update_state_custom(){
         task_in_progress = 0;
 
 
-        const message_event_status_t my_task = {robot_id, uint16_t(target[0][2]), 0, MSG_EVENT_DONE};
+        const message_event_status_t my_task = {robot_id, uint16_t(target[0][2]), MSG_EVENT_DONE};
         wb_emitter_set_channel(emitter_tag, robot_id+1);
         wb_emitter_send(emitter_tag, &my_task, sizeof(message_event_status_t));  
 
