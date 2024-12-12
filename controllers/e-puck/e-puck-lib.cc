9dcdae4dcc5a011daab5b1b73ee6a41e03f12a49
rauber6 <93669153+rauber6@users.noreply.github.com>
2024-12-12 09:04:52 +0100

    Update e-puck-lib.cc

--- controllers/e-puck/e-puck-lib.cc
+++ controllers/e-puck/e-puck-lib.cc
@@ -39,8 +39,6 @@ void Epuck::reset()
     clock = 0;
     indx = 0;
 
-    // .
-
     // Init target positions to "INVALID"
     for (i = 0; i < 99; i++)
     {
@@ -85,10 +83,9 @@ void Epuck::reset()
     // Reset stats
     stat_max_velocity = 0.0;
 }
-
-void Epuck::update_state(int _sum_distances)
+void Epuck::update_state(int _sum_distances, int _max_distance)  
 {   
-    if (_sum_distances > STATECHANGE_DIST && state == GO_TO_GOAL)
+    if ((_sum_distances > STATECHANGE_SUM || _max_distance > STATECHANGE_MAX) && (state == GO_TO_GOAL || state == OBSTACLE_AVOID))
     {
         state = OBSTACLE_AVOID;
     }
@@ -147,20 +144,29 @@ void Epuck::update_self_motion(int msl, int msr) {
 
 void Epuck::compute_avoid_obstacle(int *msl, int *msr, int distances[]) 
 {
-    int d1=0,d2=0;       // motor speed 1 and 2     
-    int sensor_nb;       // FOR-loop counters    
+    double left_weights[8] = {-2.0, -0.75, -0.2, 0, 0, 0.2, 0.75, 2.0};
+    double right_weights[8] = {2.0, 0.75, 0.2, 0, 0, -0.2, -0.75, -2.0};
+    int left_adjustment = 0;
+    int right_adjustment = 0;
+    int adjustment = 1; 
 
-    for(sensor_nb=0;sensor_nb<NB_SENSORS;sensor_nb++)
-    {   
-       d1 += (distances[sensor_nb]-300) * Interconn[sensor_nb];
-       d2 += (distances[sensor_nb]-300) * Interconn[sensor_nb + NB_SENSORS];
+    for (int sensor_nb = 0; sensor_nb < 8; ++sensor_nb) {
+        left_adjustment += distances[sensor_nb] * left_weights[sensor_nb] * 50;
+        right_adjustment += distances[sensor_nb] * right_weights[sensor_nb] * 50;
     }
-    d1 /= 8; d2 /= 8;  // Normalizing speeds
 
-    *msr = d1+BIAS_SPEED; 
-    *msl = d2+BIAS_SPEED; 
-    limit(msl,MAX_SPEED);
-    limit(msr,MAX_SPEED);
+    // Normalize adjustments and calculate motor speeds
+    *msl = BIAS_SPEED + (left_adjustment / adjustment);   // Scale down adjustments
+    *msr = BIAS_SPEED + (right_adjustment / adjustment); // Scale down adjustments
+
+    // Clamp motor speeds to the maximum allowable range
+    *msl = std::min(std::max(*msl, -MAX_SPEED), MAX_SPEED);
+    *msr = std::min(std::max(*msr, -MAX_SPEED), MAX_SPEED);
+
+    //if(*msl == 0 || *msr == 0) printf("compute_avoid_obstacle sets sth to 0\n");
+
+    //*msl = -MAX_SPEED;
+    //*msr = -MAX_SPEED;
 }
 
 void Epuck::compute_go_to_goal(int *msl, int *msr) 
@@ -195,25 +201,41 @@ void Epuck::run(int ms)
     // Motor speed and sensor variables	
     int msl=0,msr=0;                // motor speed left and right
     int distances[NB_SENSORS];  // array keeping the distance sensor readings
-    int sum_distances=0;        // sum of all distance sensor inputs, used as threshold for state change.  	
+    int sum_distances= 0 ;        // sum of all distance sensor inputs, used as threshold for state change.  	
+    int max_distance = -999;           // max reading from a single sensor, used as threshold for state change.
 
     // Other variables
     int sensor_nb;
 
     // Add the weighted sensors values
+    /*
     for(sensor_nb=0;sensor_nb<NB_SENSORS;sensor_nb++)
     {  
         distances[sensor_nb] = wb_distance_sensor_get_value(ds[sensor_nb]);
         sum_distances += distances[sensor_nb];
+    }*/
+   for(sensor_nb=4;sensor_nb<NB_SENSORS;sensor_nb++) //particular iteration for clearer display, to cleanup
+    {  
+        distances[sensor_nb] = wb_distance_sensor_get_value(ds[sensor_nb])+PS_OFFSET;
+        //if(robot_id == 2) printf("%f, ", wb_distance_sensor_get_value(ds[sensor_nb])+PS_OFFSET);
+        if(max_distance < distances[sensor_nb]) max_distance = distances[sensor_nb];
+        sum_distances += distances[sensor_nb];
     }
-
+    for(sensor_nb=0;sensor_nb<NB_SENSORS-4;sensor_nb++)
+    {  
+        distances[sensor_nb] = wb_distance_sensor_get_value(ds[sensor_nb])+PS_OFFSET;
+        //if(robot_id == 2) printf("%f, ", wb_distance_sensor_get_value(ds[sensor_nb])+PS_OFFSET);
+        if(max_distance < distances[sensor_nb]) max_distance = distances[sensor_nb];
+        sum_distances += distances[sensor_nb];
+    }
+    
     // Get info from supervisor
     receive_updates();
 
     run_custom_pre_update();
 
     // State may change because of obstacles
-    update_state(sum_distances);
+    update_state(sum_distances, max_distance);
 
     // Custom instruction
     run_custom_post_update();
